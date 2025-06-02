ea0657b1982fee0769a585951da6b7bbc9da9a76
박현도/선임연구원/B2B기업/교육솔루션개발팀 <hyundo.park@lge.com>
2025-06-03 02:27:15 +0900

    Initial commit

---
+++ checkPassword.cpp
@@ -0,0 +1,26 @@
+﻿#include <vcl.h>
+#pragma hdrstop          
+#pragma resource "*.dfm"
+#include "checkPassword.h" 
+
+__fastcall TFormPassword::TFormPassword(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+
+void __fastcall TFormPassword::btnOkClick(TObject *Sender)
+{
+    const String VALID_PASSWORD = "1234";
+
+    if (edtPassword->Text == VALID_PASSWORD) {
+        ModalResult = mrOk; // 비밀번호 맞으면 OK
+    } else {
+        ShowMessage(L"비밀번호가 틀렸습니다.");
+        edtPassword->Clear();
+        edtPassword->SetFocus();
+    }
+}
+
+void __fastcall TFormPassword::titleClick(TObject *Sender)
+{
+}
